a92a7e839ddf3e522c1f95082c692f96d80800e5
kiran patil <kirankumarpatil@gmail.com>
2018-11-25 17:02:31 -0800

    Chat App repository commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path=require('path');
+const express=require('express');
+
+const publicPath=path.join(__dirname,'../public');
+var app=express();
+
+
+console.log(__dirname+'/../public');
+console.log(publicPath);
+
+app.use(express.static(publicPath));
+app.listen(3000,()=>{
+  console.log('Server is up on port 3000');
+});
